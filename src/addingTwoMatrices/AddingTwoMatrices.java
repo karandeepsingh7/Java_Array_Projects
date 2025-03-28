3fbe2a8711a20a502330e33d76d17db38a15d1a6
work <work@localhost>
2025-03-27 21:34:46 -0400

    Adding Two Matrices

---
+++ src/addingTwoMatrices/AddingTwoMatrices.java
@@ -0,0 +1,59 @@
+package addingTwoMatrices;
+
+public class AddingTwoMatrices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int array1[][] = new int[3][3];
+		array1[0][0] = 3;
+		array1[0][1] = 5;
+		array1[0][2] = 9;
+		
+		array1[1][0] = 7;
+		array1[1][1] = 6;
+		array1[1][2] = 2;
+		
+		array1[2][0] = 4;
+		array1[2][1] = 3;
+		array1[2][2] = 5;
+		
+		int array2[][] = {{1,5,2},{6,8,4},{3,9,7}};
+		
+		for (int i = 0;i<array1.length ; i++)
+		{
+			for(int j = 0;j<array1[i].length;j++)
+			{
+				System.out.print(array1[i][j]);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for (int i = 0;i<array2.length ; i++)
+		{
+			for(int j = 0;j<array2[i].length;j++)
+			{
+				System.out.print(array2[i][j]);
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		int array3[][] = new int[3][3];
+		
+		for (int i = 0;i<array3.length ; i++)
+		{
+			for(int j = 0;j<array3[i].length;j++)
+			{
+				array3[i][j] = array1[i][j] + array2[i][j];
+				System.out.print(array3[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
